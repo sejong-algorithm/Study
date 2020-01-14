80a3367b87dcda99c471cbdf74466a55c2ade816
Zojae031 <edgar3@naver.com>
2020-01-14 18:15:26 +0900

    3주차 코드

---
+++ zojae031/3week/src/쇠막대기.kt
@@ -0,0 +1,50 @@
+//import java.util.*
+//
+//fun main() {
+//    Solution().run {
+//        solution("()(((()())(())()))(())")
+//            .also { println(it) }
+//    }
+//}
+//
+//class Solution {
+//    fun solution(arrangement: String): Int {
+//        var answer = 0
+//        val lasers = mutableListOf<Int>()
+//        val stack = Stack<Pair<Char, Int>>()
+//
+//        for (i in arrangement.indices) {
+//
+//            if (stack.isNotEmpty()) {
+//
+//                if (stack.peek().first == '(' && arrangement[i] == ')') {
+//
+//                    if (stack.peek().second == i - 1) {//레이저 검출
+//                        lasers.add(i)
+//                    } else {//쇠 파이프 검출
+//                        var cnt = 0
+//
+//                        for (laser in lasers) {
+//                            //쇠막대기 시작 인덱스 {stack.peek().second}
+//                            //쇠막대기 끝 인덱스 { i } 안에 laser 가 존재하는지 확인
+//                            if (laser > stack.peek().second && laser < i) {
+//                                cnt++
+//                            }
+//                        }
+//
+//                        if (cnt != 0) {//잘렸다면 잘린 개수 +1 개 만큼 더 생기니까 +1
+//                            answer += cnt + 1
+//                        }
+//
+//                    }
+//                    stack.pop()
+//                    continue
+//                }
+//            }
+//
+//            stack.push(arrangement[i] to i)
+//
+//        }
+//        return answer
+//    }
+//}
---
+++ zojae031/3week/src/프린터.kt
@@ -0,0 +1,47 @@
+//fun main() {
+//    Solution().run {
+//        solution(intArrayOf(2, 1, 3, 2), 2)
+//            .also { println(it) }//1
+//
+//        solution(intArrayOf(1, 1, 9, 1, 1, 1), 0)
+//            .also { println(it) }//5
+//    }
+//}
+//
+//class Solution {
+//    fun solution(priorities: IntArray, location: Int): Int {
+//        var answer = 1
+//        /** Queue 형태로 만들어주는 작업
+//         * first : 2 1 3 2
+//         * second : 0 1 2 3
+//         */
+//        val queue: MutableList<Pair<Int, Int>> = priorities.mapIndexed { index, i ->
+//            i to index
+//        }.toMutableList()
+//
+//        while (queue.isNotEmpty()) {
+//            /**
+//             * max : 최대값을 구함
+//             * value : 큐의 맨 앞부분
+//             */
+//            val max = queue.maxBy { it.first }!!
+//            val value = queue.first()
+//
+//            when {// 순서가 굉장히 중요함
+//                value.first < max.first -> { //최대값보다 작으면 뒤로 돌림
+//                    queue.remove(value)
+//                    queue.add(value)
+//                }
+//                value.first == max.first && value.second != location -> { //제일 큰값이면서 원하는 값이 아니면
+//                    queue.remove(value)//제거 후
+//                    answer++//카운트 증가
+//                }
+//                value.second == location -> {//원하는 값이 나온다면 출력
+//                    return answer
+//                }
+//            }
+//
+//        }
+//        return answer
+//    }
+//}
