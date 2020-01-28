3158a8fe38f629b2846089f85214d02260d55260
Zojae031 <edgar3@naver.com>
2020-01-28 14:42:13 +0900

    Zojae0 commit

---
+++ zojae031/Algorithm/src/4week/다리를 지나는 트럭.kt
@@ -0,0 +1,37 @@
+package `4week`
+
+import java.util.*
+
+class Solution {
+    fun solution(bridge_length: Int, weight: Int, truck_weights: IntArray): Int {
+        var answer = 0
+        val readyQueue: Queue<Int> = LinkedList()
+        val goingQueue: Queue<Pair<Int, Int>> = LinkedList()
+
+        var nowWeight = 0
+
+        truck_weights.forEach {
+            readyQueue.add(it)
+        }
+
+        while (readyQueue.isNotEmpty()) {
+            if (goingQueue.isNotEmpty() && answer - goingQueue.peek().second == bridge_length) {
+                nowWeight -= goingQueue.remove().first
+            }
+            if (weight - nowWeight - readyQueue.peek() >= 0) {
+                nowWeight += readyQueue.remove().apply {
+                    goingQueue.add(this to answer)
+                }
+            }
+            answer++
+        }
+
+        while (goingQueue.isNotEmpty()) {
+            if (answer - goingQueue.peek().second == bridge_length) {
+                goingQueue.remove()
+            }
+            answer++
+        }
+        return answer
+    }
+}
--- zojae031/Algorithm/src/4week/멀쩡한 사각형.kt
+++ zojae031/Algorithm/src/4week/멀쩡한 사각형.kt
@@ -1,6 +1,6 @@
 fun main() {
     Solution().run {
-        solution(8, 12)
+        solution(100000000, 99999999)
             .also { println(it) }//80
     }
 }
@@ -8,7 +8,36 @@ fun main() {
 class Solution {
     fun solution(w: Int, h: Int): Long {
         var answer: Long = 0
+        val min = Integer.min(w, h).toLong()
+        val max = Integer.max(w, h).toLong()
 
-        return answer
+        when {
+            max == min -> {//정사각형이면 그대로 반환
+                answer = min
+            }
+            min.toInt() % 2 == 0 -> {//짝수 일때
+                answer = if (max.toInt() % min.toInt() == 0) {//나누어 떨어지는 경우
+                    max
+                } else {
+                    val cal = max / min + 1
+                    if (max % min < min / 2) {//같은값들
+                        (min * cal)
+                    } else {//다른값들
+                        (min * cal) + (max % min - min / 2) * 2
+                    }
+
+                }
+            }
+            else -> {//홀수 일때
+                answer = if (max.toInt() % min.toInt() == 0) {//나누어 떨어지는 경우
+                    max
+                } else {//그렇지 않은 경우
+                    val cal = max / min + 1
+                    (min * cal + (max % min - 1))
+                }
+            }
+        }
+
+        return max * min - answer
     }
 }
